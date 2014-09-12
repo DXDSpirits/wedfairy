a31d608b41a5325b07ad940fea4f4786f0e40ec0
Xindong Ding <dxd.spirits@gmail.com>
2014-09-12 13:36:34 +0800

    modify error page

--- app.js
+++ app.js
@@ -51,31 +51,12 @@ app.use(function(req, res, next) {
 
 /// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {
-                status: err.status || 500,
-                stack: err.stack
-            }
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {
-            status: err.status || 500,
-            stack: ''
-        }
+        status: err.status || 500,
+        stack: err.stack
     });
 });
 
