f82e36ead28ca9a409152de00668263e78d97831
Xindong Ding <dxd.spirits@gmail.com>
2014-09-12 11:26:13 +0800

    Fix path issue of requirejs optimizer

--- build.js
+++ build.js
@@ -3,9 +3,9 @@ var requirejs = require('requirejs');
 
 var build = function() {
     requirejs.optimize({
-        baseUrl: 'assets/javascripts/storytell',
+        baseUrl: __dirname + '/assets/javascripts/storytell',
         name: '../storytell',
-        out: 'assets/javascripts/storytell-built.js'
+        out: __dirname + '/assets/javascripts/storytell-built.js'
     }, function (buildResponse) {
         console.log(buildResponse);
     }, function(err) {
