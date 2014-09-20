893a00bd274b4019786f088143ab60db3c42a8da
Xindong Ding <dxd.spirits@gmail.com>
2014-09-21 02:25:28 +0800

    style homepage howto section (slider carousel)

--- assets/javascripts/homepage.js
+++ assets/javascripts/homepage.js
@@ -1,4 +1,27 @@
 (function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+})();
+
+(function() {
     
     $('#view-hero').css({
         'min-height': $(window).height(),
@@ -9,14 +32,14 @@
         var target = $(e.currentTarget).attr('href');
         if (target && target[0] == '#') {
             e.preventDefault && e.preventDefault();
-            $('body').animate({
+            $('body,html').animate({
                 scrollTop: $(target).offset().top
             }, 1000);
         }
     });
     
     var onScroll = _.throttle(function() {
-        $('#global-navbar').toggleClass('hidden', window.scrollY < $(window).height());
+        $('#global-navbar').toggleClass('hidden', $(window).scrollTop() < $(window).height());
     }, 500);
     
     $(window).scroll(onScroll);
@@ -70,6 +93,100 @@
             ])
         }
     }))({el: $('#view-gallery')});
+    
+    SectionViews['howto'] = new (Amour.View.extend({
+        events: {
+            'click .slider-item': 'slideOnClick'
+        },
+        initView: function() {},
+        slideOnClick: function(e) {
+            var $item = $(e.currentTarget);
+            var index = $item.prevAll().length;
+            if (index == this.mean) return;
+            var dir = index > this.mean ? 'slideLeft' : 'slideRight';
+            var dis = Math.abs(index - this.mean);
+            var speed = 600 / dis;
+            var self = this;
+            var animate = _.before(dis + 1, function() {
+                self[dir](speed, animate);
+            });
+            this.$('.slider-item').removeClass('standout');
+            animate();
+            _.delay(function() {
+                $item.addClass('standout');
+            }, 600);
+        },
+        calculateStyle: function(index) {
+            var fullWidth = this.itemWidth + this.gap;
+            var opacity = (index < 0 || index >= this.n) ? 0 : 1;
+            if (index < this.mean) {
+                return {
+                    'width': this.itemWidth,
+                    'height': this.itemWidth,
+                    'top': this.itemWidth / 2,
+                    'left': index * fullWidth,
+                    'opacity': opacity
+                };
+            } else if (index > this.mean) {
+                return {
+                    'width': this.itemWidth,
+                    'height': this.itemWidth,
+                    'top': this.itemWidth / 2,
+                    'left': this.gap + (index + 1) * fullWidth,
+                    'opacity': opacity
+                };
+            } else {
+                return {
+                    'width': this.itemWidth * 2,
+                    'height': this.itemWidth * 2,
+                    'top': 0,
+                    'left': this.gap + this.mean * fullWidth,
+                    'opacity': opacity
+                };
+            }
+        },
+        slideRight: function(speed, callback) {
+            var $lastItem = $('.slider-item:last-child');
+            var style = this.calculateStyle(-1);
+            $lastItem.clone().css(style).prependTo('.slider-wrapper');
+            var animationEnd = _.after(this.n + 1, function() {
+                $lastItem.remove();
+                callback && callback();
+            });
+            var self = this;
+            this.$('.slider-item').each(function(index) {
+                var style = self.calculateStyle(index);
+                $(this).animate(style, speed || 1000, animationEnd);
+            });
+        },
+        slideLeft: function(speed, callback) {
+            var $firstItem = $('.slider-item:first-child');
+            var style = this.calculateStyle(this.n + 1);
+            $firstItem.clone().css(style).appendTo('.slider-wrapper');
+            var animationEnd = _.after(this.n + 1, function() {
+                $firstItem.remove();
+                callback && callback();
+            });
+            var self = this;
+            this.$('.slider-item').each(function(index) {
+                var style = self.calculateStyle(index - 1);
+                $(this).animate(style, speed || 1000, animationEnd);
+            });
+        },
+        render: function() {
+            var gap = this.gap = 10;
+            var n = this.n = this.$('.slider-item').length;
+            var mean = this.mean = (n >> 1);
+            var wrapperWidth = this.wrapperWidth = this.$('.slider-wrapper').innerWidth();
+            var itemWidth = this.itemWidth = (wrapperWidth - (n + 1) * gap) / (n + 1);
+            this.$('.slider-wrapper').css('height', itemWidth * 2);
+            var self = this;
+            this.$('.slider-item').each(function(index) {
+                var style = self.calculateStyle(index);
+                $(this).css(style);
+            });
+        }
+    }))({el: $('#view-howto')});
     
     var fillImages = function() {
         $('img[data-src]').each(function() {
