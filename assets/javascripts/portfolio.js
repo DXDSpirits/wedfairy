e6101fd2e299891b2437eb670ec52c01ff141f4a
Xindong Ding <dxd.spirits@gmail.com>
2014-12-30 12:01:07 +0800

    add date and time to portfolio

--- assets/javascripts/portfolio.js
+++ assets/javascripts/portfolio.js
@@ -7,8 +7,14 @@
             },
             className: 'story-item text-center col-xs-6 col-sm-3 col-md-2',
             template: '<div class="cover img" data-bg-src="{{data.coverImage}}"></div>' +
-                      '<p class="name">{{name}} (<strong>{{progress}}</strong>)</p><p>{{owner}}</p>' +
-                      '<p class="title">{{title}}</p>', 
+                      '<p class="small">{{formatted_date}}</p>' + 
+                      '<p class="name">{{name}}<br>(<strong>{{progress}}</strong>)</p><p>{{owner}}</p>' +
+                      '<p class="title">{{title}}</p>',
+            serializeData: function() {
+                var data = this.model ? this.model.toJSON() : {};
+                data.formatted_date = (new Date(data.time_created)).toLocaleString();
+                return data;
+            },
             onClick: function() {
                 window.open('http://wedfairy.com/story/' + this.model.get('name'), '_blank');
             }
