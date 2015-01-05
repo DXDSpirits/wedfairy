d6b972bd8976af8c514abccc9730a03eebf8d6f3
Xindong Ding <dxd.spirits@gmail.com>
2015-01-05 17:06:35 +0800

    add feature and unfeature api to story portfolio page

--- assets/javascripts/portfolio.js
+++ assets/javascripts/portfolio.js
@@ -3,12 +3,15 @@
     var StoryGalleryView = Amour.CollectionView.extend({
         ModelView: Amour.ModelView.extend({
             events: {
-                'click .cover': 'onClick'
+                'click .cover': 'onClick',
+                'click .fa-check-circle': 'feature'
             },
             className: 'story-item text-center col-xs-6 col-sm-3 col-md-2',
             template: '<div class="cover img" data-bg-src="{{data.coverImage}}"></div>' +
-                      '<p class="small">{{formatted_date}}</p>' + 
-                      '<p class="name">{{name}}<br>(<strong>{{progress}}</strong>)</p><p>{{owner}}</p>' +
+                      '<p class="small">{{formatted_date}}</p>' +
+                      '<p class="name">{{name}}</p>' +
+                      '<p class="{{#featured}}text-primary{{/featured}}">{{progress}} <i class="fa fa-lg fa-check-circle"></i></p>' +
+                      '<p>{{owner}}</p>' +
                       '<p class="title">{{title}}</p>',
             serializeData: function() {
                 var data = this.model ? this.model.toJSON() : {};
@@ -17,13 +20,26 @@
             },
             onClick: function() {
                 window.open('http://wedfairy.com/story/' + this.model.get('name'), '_blank');
+            },
+            feature: function(e) {
+                if (!this.model.get('featured')) {
+                    this.model.save({}, {
+                        url: this.model.url() + 'feature/'
+                    });
+                } else {
+                    this.model.save({}, {
+                        url: this.model.url() + 'unfeature/'
+                    });
+                }
             }
         })
     });
     
     var stories = new (Amour.Collection.extend({
-        url: Amour.APIHost + '/sites/storylist/',
-        model: Amour.Models.Story
+        url: Amour.APIHost + '/staff/story/',
+        model: Amour.Models.Story.extend({
+            urlRoot: Amour.APIHost + '/staff/story/'
+        })
     }))();
     
     var storyGalleryView = new StoryGalleryView({
@@ -33,7 +49,7 @@
     
     stories.fetch();
     
-    $('#btn-more').click(function () {
+    $('#btn-more').on('click', function () {
         var btn = $(this);
         btn.button('loading');
         stories.fetchNext({
@@ -42,6 +58,23 @@
                 btn.button('reset');
             }
         });
+    });
+    
+    $('input[name=featured]').on('change', function() {
+        if ($('input[name=featured]:checked').val() == 'on') {
+            stories.fetch({
+                reset: true,
+                data: { featured: 'True' }
+            });
+        } else {
+            stories.fetch({
+                reset: true
+            });
+        }
+    });
+    
+    Amour.ajax.on('unauthorized', function() {
+        
     });
     
 })();
