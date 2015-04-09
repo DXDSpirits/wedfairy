33e46aa1fe43211d3c810faccc58ed57566fc3a7
Yang <zhy0216@gmail.com>
2015-04-09 17:49:01 +0800

    import js from ranking

--- assets/javascripts/explore.js
+++ assets/javascripts/explore.js
@@ -1,10 +1,41 @@
-$(function(){
+$(function() {
+
+
+    var StoryGalleryView = Amour.CollectionView.extend({
+        ModelView: Amour.ModelView.extend({
+            events: {
+                'click': 'onClick'
+            },
+            className: 'col-md-3 story-item',
+            template: $("#explore-story-template").html(),
+            serializeData: function() {
+                var data = this.model ? this.model.toJSON() : {};
+                data.formatted_date = (new Date(data.time_created + '+0800')).toLocaleString();
+                return data;
+            },
+            onClick: function() {
+                window.open('http://wedfairy.com/story/' + this.model.get('name'), '_blank');
+            }
+        })
+    });
+
+    var stories = new(Amour.Collection.extend({
+        url: Amour.APIRoot + 'sites/storylist/',
+        model: Amour.Models.Story
+    }))();
+
+    var storyGalleryView = new StoryGalleryView({
+        collection: stories,
+        el: $('.story-list')
+    });
+
+    stories.fetch();
+    stories.on('reset add', function() {
+        $('#btn-more').toggleClass('hidden', stories.next == null);
+    });
 
 
 
 
 
-
-
-    
 })
