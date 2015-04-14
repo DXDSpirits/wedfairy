88733e7facc91042f7bf03ccd276565babd5de8d
Xindong Ding <dxd.spirits@gmail.com>
2015-04-14 15:26:29 +0800

    fix explore.js

--- assets/javascripts/explore.js
+++ assets/javascripts/explore.js
@@ -1,171 +1,111 @@
 $(function() {
 
-
-
-    var isFetching = false;
-    var sceneFilter = null;
-
-    _.repeat = function(func, wait) {
-        var wait = wait || 1000;
-
-        return function _wrapper() {
-            func();
-            _.delay(function() {
-                _wrapper();
-            }, wait);
-        }
-    }
-    
-
     var StoryGalleryView = Amour.CollectionView.extend({
         ModelView: Amour.ModelView.extend({
             events: {
                 'click .story-cover': 'onClick'
             },
-            className: 'col-xs-12 col-sm-4 col-md-3 story-item',
+            className: 'animated fadeIn col-sm-4 col-md-3 story-item',
             template: $("#explore-story-template").html(),
-
             serializeData: function() {
                 var data = this.model ? this.model.toJSON() : {};
                 data.formatted_date = (new Date(data.time_created + '+0800')).toLocaleString();
                 data.likes = data.likes || 0
-                data.views = (data.likes * 3 + data.comments * 7) | 0;
+                data.views = (data.likes * 3 + data.comments * 7) || 0;
                 return data;
             },
             onClick: function() {
                 window.open('http://wedfairy.com/story/' + this.model.get('name'), '_blank');
             },
-        }),
-        addOne: function(item){
-            console.log("add one");
-            var self = this;
-            _.delay(function(){
-                Amour.CollectionView.prototype.addOne.call(self, item);
-                $(".loading").css("visibility", "hidden");
-            }, 500)
-        },
-       
+        })
     });
 
-    var stories = new(Amour.Collection.extend({
+    var stories = new (Amour.Collection.extend({
         url: Amour.APIRoot + 'sites/storylist/',
         model: Amour.Models.Story
     }))();
-
 
     var storyGalleryView = new StoryGalleryView({
         collection: stories,
         el: $('.story-container')
     });
 
-
-    Backbone.on('next-page', function(){
-        $(".loading").css("visibility", "visible");
- 
-        if(stories.next ){
-            needRender = false;
-            isFetching = true;
+    stories.on('reset add', function() {
+        $(".loading").toggleClass("hidden", stories.next == null);
+    });
+    var isFetching = false;
+    Backbone.on('next-page', function() {
+        if (isFetching) return;
+        isFetching = true;
+        if (stories.next) {
             stories.fetchNext({
                 remove: false, 
-                // data: {schema: sceneFilter},
                 success: function() {
+                    isFetching = false;
+                },
+                error: function() {
                     isFetching = false;
                 }
             });    
         }
+    });
 
-        if(!stories.next){
-            $(".loading").css("visibility", "hidden");
-        }
-
-    })
-
-    Backbone.on("close-scene-filter-menu", (function(){
+    Backbone.on("close-scene-filter-menu", (function() {
         var $menu = $('.scene-filter-menu');
-        return function(){
+        return function() {
             $menu.hide();
         }
     })());
 
-
-    $(document).on('click', '.scene-filter .selected-scene', function(){
+    $(document).on('click', '.scene-filter .selected-scene', function() {
         var $menu = $('.scene-filter-menu');
-        if($menu.is(":visible")){
+        if ($menu.is(":visible")) {
             Backbone.trigger("close-scene-filter-menu");
-        }else{
+        } else {
             $("html").off("click");
-            $("html").on("click", function(e){
+            $("html").on("click", function(e) {
                 var $target = $(e.target);
-                if($target.closest(".scene-filter").length == 0 && $menu.is(":visible")){
+                if ($target.closest(".scene-filter").length == 0 && $menu.is(":visible")) {
                     Backbone.trigger("close-scene-filter-menu");
                     $("html").off("click");
                 }
             });
             $menu.show();
         }
-
-    })
-
+    });
 
     // infinite scroll
-    // Backbone.on("render", function(){
-    //     renderStories.reset(stories.models);
-    // })
-
-
-    var throttle = function() {
-        var scrollTop = $(window).scrollTop();
-        if(isFetching){
-            console.log("delay");
-            return ;
-        }
-        if ($(window).scrollTop() + $(window).height() >= $('body').height() - 150) {
-            // console.log("trigger");
-            // console.log("isFetching:"+ isFetching);
+    var throttle = _.throttle(function() {
+        if ($(window).scrollTop() + $(window).height() >= $('body').height() - 260) {
             Backbone.trigger('next-page');
         }
-    };
-    var _run = _.repeat(throttle, 200);
-    _run();
-
-
+    }, 200);
+    $(window).scroll(throttle);
 
     // backbone router stuff
     var ROUTER = new (Backbone.Router.extend({
-
-        routes: {':schemaFilter': 'schemaFilter',
-                '': 'schemaFilter'},
-        schemaFilter: function(filterName){
+        routes: {
+            ':schemaFilter': 'schemaFilter',
+            '': 'schemaFilter'
+        },
+        schemaFilter: function(filterName) {
             var filterName = filterName || "all";
             var $selectedScene = $('.selected-scene');
-            // console.log(filterName);
             $('.scene-filter-menu').hide();
             $('.scene-filter-menu a').removeClass('active');
             var $temp = $('.scene-filter-menu [filter-name=' + filterName +"]");
             $temp.addClass("active");
             $selectedScene.find(".text").html($temp.html());
-            
-            if(filterName == "all"){ // 'all' means we do not need schema-filter
+            if (filterName == "all") { // 'all' means we do not need schema-filter
                 filterName = null;
             }
-            sceneFilter = filterName;
             stories.fetch({
-                data:{schema: filterName},
-                reset: true,
-                success: function(){
-                    // Backbone.trigger("render")
-                }                
+                data: { schema: filterName },
+                reset: true
             });
-
         }
-    }))()
+    }))();
 
+    Backbone.history.start();
 
-    Backbone.history.start()
-
-})
-
-
-
-
-
+});
