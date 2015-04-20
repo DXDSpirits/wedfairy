cb69563bf2134d1e877f9019a56f01c252768221
zhy0216 <zhy0216@gmail.com>
2015-04-20 20:26:49 +0800

    finish login part

--- assets/javascripts/header.js
+++ assets/javascripts/header.js
@@ -1,47 +1,106 @@
-$(function(){
+$(function() {
 
     var self = this;
     var user = new Amour.Models.User();
-    
 
-    function isLogin(){
+
+    function isLogin() {
         return !!localStorage.getItem('auth-token');
     }
 
-    function initPage(){
+    function _renderRightNav(){
+        if (isLogin()) {
+            $('.right-nav').addClass("view-hide");
+            $('.avatar-container').removeClass("view-hide");
+        }else{
+            $('.right-nav').removeClass("view-hide");
+            $('.avatar-container').addClass("view-hide");
+        }
+    }
 
-        if(isLogin()){
+    function initPage() {
+
+        if (isLogin()) {
             user.fetch({
                 success: function() {
                     $(".show-mobile-number").html("手机号: " + user.get('username'));
+                    _renderRightNav();
                 }
             })
-        }else{
-
-
         }
 
-        $('.user-menu-btn').on('click', function(){
+
+        Backbone.on("login-user", function() {
+            var username = $('#modal-container .login-form .username-input').val() || null;
+            var password = $('#modal-container .login-form .password-input').val() || null;
+            if (username && password) {
+                user.login({
+                    username: username,
+                    password: password
+                }, {
+                    "error": function() {
+                        alert('手机号或者密码错误，请重新输入');
+                    },
+                    "success": function() {
+                        $('#modal-container').html("登录成功");
+                        _.delay(function(){
+                            _renderRightNav();
+                            Backbone.trigger('close-modal');
+                        }, 500);
+
+                    },
+                })
+            } else {
+                alert('请输入手机号和密码!');
+            }
+        });
+
+
+        $(document).on('click', '#login-modal .login-btn', function() {
+            Backbone.trigger('login-user');
+        })
+
+        $('.user-menu-btn').on('click', function() {
             $('.header .user-menu').toggleClass('view-hide');
         })
 
-        $(document).on('click', '.login-btn', function(){
+        $(document).on('click', '.login-btn', function() {
             var $container = $('#modal-container');
             $container.html($('#login-modal').clone());
             $container.fadeIn();
         });
 
-        $('.logout-btn').on('click', function(){
-            Amour.TokenAuth.clear();
-            window.location.href = '/login';
+        $(document).on('click', '.register-btn', function() {
+            var $container = $('#modal-container');
+            $container.html($('#register-modal').clone());
+            $container.fadeIn();
+        });
+
+        Backbone.on('close-modal', function(){
+            var $container = $('#modal-container');
+            $container.fadeOut(function() {
+                $container.html('');
+            });
         })
+
+        $(document).on('click', '#modal-container .close', function() {
+            Backbone.trigger('close-modal');
+        })
+
+        $('.logout-btn').on('click', function() {
+            $('.header .user-menu').addClass('view-hide');
+            Amour.TokenAuth.clear();
+            _renderRightNav();
+        })
+
+        $(document).on('keydown', '#login-modal .password-input,' +
+                                  '#register-modal .password-input2',function(e){
+            if (13 == e.keyCode) {  // 27 is the ESC key
+                Backbone.trigger('login-user');
+            }
+        });
     }
 
-
-
-    
-
-    
 
 
 
