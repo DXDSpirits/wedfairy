554ab8bfae9a2626c88f1de7b4e8eeacb5c6ad00
Xindong Ding <dxd.spirits@gmail.com>
2015-06-25 02:35:50 +0800

    switch homepage to /newhome

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('home');
+    //res.render('home');
+    res.render('homepage');
 });
 
 router.get('/newhome', function(req, res) {
