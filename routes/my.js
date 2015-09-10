8eaebcc48acfd2ec117f9d2d9bbf8b79554f6912
Ran <lr_naril@163.com>
2015-09-10 12:24:06 +0800

    rename mygallery to my/gallery

--- routes/my.js
+++ routes/my.js
@@ -1,6 +1,7 @@
-
+var settings = require('../settings/settings.js');
 var express = require('express');
 var router = express.Router();
+var qiniu = require('qiniu');
 
 var routes = {
     '': 'my',
@@ -11,21 +12,8 @@ router.get('/', function(req, res) {
     res.render('my/my');
 });
 
-router.get('/mygallery', function(req, res) {
+router.get('/gallery', function(req, res) {
     res.render('my/mygallery');
 });
 
-
-// router.get('/:name', function(req, res) {
-//     var name = req.params.name;
-//     res.render('my/' + routes[name||'']);
-// });
-
- 
-router.get('/:filterName', function(req, res) {
-    var filterName = req.params.filterName;
-    res.render('my/my', {
-        filterName: filterName
-    });
-});
 module.exports = router;
