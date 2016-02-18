064b4303a62b466197a6e5ce3a91673392502a0b
Xindong Ding <dxd.spirits@gmail.com>
2016-02-18 23:37:48 +0800

    update package versions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('newrelic');
 var settings = require('./settings/settings');
 var express = require('express');
 var path = require('path');
-//var favicon = require('static-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
